d4b61102604908aa93f24d878017a86a8583c419
CeskKim <pkuuu@naver.com>
2021-03-16 22:07:23 +0900

    Add files via upload

---
+++ SQL Server/Monitoring/QueryStore(Long, IO Query) Search.sql
@@ -0,0 +1,34 @@
+/*************************************************************************************************
+1시간이내 평균 실행 시간이 가장 긴 쿼리
+**************************************************************************************************/
+SELECT TOP 10 rs.avg_duration, qt.query_sql_text, q.query_id,
+    qt.query_text_id, p.plan_id, GETUTCDATE() AS CurrentUTCTime,
+    rs.last_execution_time
+FROM sys.query_store_query_text AS qt
+JOIN sys.query_store_query AS q
+    ON qt.query_text_id = q.query_text_id
+JOIN sys.query_store_plan AS p
+    ON q.query_id = p.query_id
+JOIN sys.query_store_runtime_stats AS rs
+    ON p.plan_id = rs.plan_id
+WHERE rs.last_execution_time > DATEADD(HOUR, -1, GETUTCDATE())
+ORDER BY rs.avg_duration DESC;
+
+
+/*************************************************************************************************
+24시간동안 평균 물리적 I/O 읽기가 가장 큰 쿼리 수, 평균 행 수, 실행 수
+**************************************************************************************************/
+SELECT TOP 10 rs.avg_physical_io_reads, qt.query_sql_text,
+    q.query_id, qt.query_text_id, p.plan_id, rs.runtime_stats_id,
+    rsi.start_time, rsi.end_time, rs.avg_rowcount, rs.count_executions
+FROM sys.query_store_query_text AS qt
+JOIN sys.query_store_query AS q
+    ON qt.query_text_id = q.query_text_id
+JOIN sys.query_store_plan AS p
+    ON q.query_id = p.query_id
+JOIN sys.query_store_runtime_stats AS rs
+    ON p.plan_id = rs.plan_id
+JOIN sys.query_store_runtime_stats_interval AS rsi
+    ON rsi.runtime_stats_interval_id = rs.runtime_stats_interval_id
+WHERE rsi.start_time >= DATEADD(hour, -24, GETUTCDATE())
+ORDER BY rs.avg_physical_io_reads DESC;
