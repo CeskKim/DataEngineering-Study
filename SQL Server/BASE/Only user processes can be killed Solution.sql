db1e83488056d13828eaa0990067bee465ca2d40
CeskKim <pkuuu@naver.com>
2021-03-23 22:05:50 +0900

    Add files via upload

---
+++ SQL Server/BASE/Only user processes can be killed Solution.sql
@@ -0,0 +1,6 @@
+/*************************************************************************************************
+ Only user processes can be killed Solution
+**************************************************************************************************/
+ALTER  DATABASE [µðºñ¸íÄª] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+GO
+ALTER  DATABASE [µðºñ¸íÄª] SET MULTI_USER
