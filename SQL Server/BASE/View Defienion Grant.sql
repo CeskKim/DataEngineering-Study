c4988f39bffafbf9cfa6ad9cf224a596b0c05ea7
CeskKim <pkuuu@naver.com>
2021-06-01 21:55:45 +0900

    Create View Defienion Grant.sql

---
+++ SQL Server/BASE/View Defienion Grant.sql
@@ -0,0 +1,7 @@
+ -- View Defienion 권한 부여
+ 
+ ##############################################################################
+ 참조사이트
+ https://docs.microsoft.com/ko-kr/sql/relational-databases/security/permissions-database-engine?view=sql-server-ver15
+ https://www.sqlshack.com/an-overview-of-a-view-definition-permission-in-sql-server/
+ ##############################################################################
