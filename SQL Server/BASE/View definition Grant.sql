c21c928e3f589ff692fbee16134a663502473662
CeskKim <pkuuu@naver.com>
2021-06-07 22:49:29 +0900

    Update and rename View Defienion Grant.sql to View definition Grant.sql

--- SQL Server/BASE/View definition Grant.sql
+++ SQL Server/BASE/View definition Grant.sql
@@ -1,7 +1,11 @@
- -- View Defienion 권한 부여
+ -- View definition 권한 부여
  
  ##############################################################################
  참조사이트
  https://docs.microsoft.com/ko-kr/sql/relational-databases/security/permissions-database-engine?view=sql-server-ver15
  https://www.sqlshack.com/an-overview-of-a-view-definition-permission-in-sql-server/
  ##############################################################################
+
+
+ ## VIEW definition의 경우 Master범위에서 설정 가능
+ ## 계정생성 -> master -> public 연결 -> master내에서 VIEW definition 설정
